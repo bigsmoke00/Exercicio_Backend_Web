9e578a5b7525e526f639c7ad07b2b552e421c4e1
bigsmoke00 <geraldosousa30@gmail.com>
2022-10-27 23:23:20 -0300

    att

---
+++ Exercício - Backend Web/src/index.js
@@ -0,0 +1,15 @@
+import Express from 'Express';
+
+const app = Express();
+
+app.get('/', (req, res) =>
+    res.send("<h3>Meu nome Completo</h3><p>Geraldo de Sousa Cruz Neto")
+);
+
+app.get('/users/:name', (req, res) =>
+    res.send('Usuário:' + req.params.name)
+);
+
+app.listen(3000, () =>
+    console.log('Servidor iniciado na porta 3000')
+);
